5771b9fbd316b4acba72ad682394f93b083ef730
Bian Klancnik <bian.klancnik@gmail.com>
2021-11-16 12:30:37 +0100

    Update quote

--- src/modules/quotes/quotes.service.ts
+++ src/modules/quotes/quotes.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateQuoteDTO } from './dto/create-quote.dto';
+import { UpdateQuoteDTO } from './dto/update-quote.dto';
 import { Quote } from './quote.entity';
 import { QuotesRepository } from './quotes.repository';
 
@@ -28,5 +29,24 @@ export class QuotesService {
     const result = await this.quotesRepository.delete(id);
     if (result.affected === 0)
       throw new NotFoundException('Quote with ID ' + id + ' not found');
+  }
+
+  async updateQuote(
+    id: string,
+    updateQuoteDTO: UpdateQuoteDTO,
+  ): Promise<Quote> {
+    const task = await this.getQuoteById(id);
+
+    const { title, desc } = updateQuoteDTO;
+    if (typeof title !== 'undefined') {
+      task.title = title;
+    }
+    if (typeof desc !== 'undefined') {
+      task.desc = desc;
+    }
+
+    await this.quotesRepository.save(task);
+
+    return task;
   }
 }
